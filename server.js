2a75ce49ad540568a8b881ee9810f50fcf820f86
Matt McIntyre <mgmcintyre777@gmail.com>
2018-03-01 14:43:24 -0500

    b

--- server.js
+++ server.js
@@ -4,7 +4,5 @@ const app = require("./app");
 // allow environment to set port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(
-    "App listening on: " + port + " @ " + new Date().toLocaleString()
-  );
+  console.log("App listening on " + port + " @ " + new Date().toLocaleString());
 });
